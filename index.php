eb820f97637f7857cd9e01b37a70ee45bc5c948d
livia128924 <livia128924@gmail.com>
2023-01-26 15:15:03 -0300

    atualizando

--- index.php
+++ index.php
@@ -58,10 +58,11 @@
 </div>
 
   <!-- ======= Header ======= -->
+  
 
   <section id="hero-animated" class="hero-animated d-flex align-items-center mt-3">
     <div class="container d-flex flex-column justify-content-center align-items-center text-center position-relative" data-aos="zoom-out">
-        <h2 style="font-size:38px;color:#eee; box-shadow: 2px 2px 5px #000;font-weight: 800"><b>Chá 
+        <h2 style="font-size:50px;color:#eee; text-shadow: 2px 2px 5px #000;font-weight: 800"><b>Chá 
           Revelação </b></h2>
         <img src="assets/img/cegonha2.png" class="img-fluid animated">
        
