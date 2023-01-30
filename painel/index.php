bb51aee2075227eb3c08584330fab46b0b736423
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-30 14:11:51 -0400

    Atualizações

--- painel/index.php
+++ painel/index.php
@@ -19,7 +19,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/x-icon" href="img/icone.png">
-    <title>Barbearia Os Manos</title>
+    <title>CEGONHA</title>
     <?php
     include("lib/header.php");
     ?>
