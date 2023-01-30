04e06200f7714023d839bbf49e44b3d25513f037
tamerelmenoufi <tamer@mohatron.com.br>
2023-01-30 14:18:17 -0400

    Atualizações

--- painel/src/componentes/menu_lateral/menu.php
+++ painel/src/componentes/menu_lateral/menu.php
@@ -21,11 +21,11 @@
 
 <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
   <div class="offcanvas-header">
-    <img src="img/logo-topo.png" style="height:100px;" alt="">
+    <img src="img/icone.png" style="height:100px;" alt="">
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
   <div class="offcanvas-body">
-    <h5>Os Manos Barbearia</h5>
+    <!-- <h5>Cegonha</h5> -->
 
     <div class="row mb-1 menu-cinza">
       <div class="col">
