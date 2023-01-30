2a71ef9d4751cdc532d9cd4de5499f65ef452765
livia128924 <livia128924@gmail.com>
2023-01-30 16:23:14 -0300

    atualizando

--- endereco.php
+++ endereco.php
@@ -2,16 +2,20 @@
  style="height:100px; margin-right:0px;"  class="img-fluid animated">
 
  <p style="color:#807d86;font-size:22px">
-Ola,você entre muitas pessoas<br> foi escolhido(a) para <br>participar dessa descoberta <br>junto com meus papais.<br>
-Conto com sua presença!
+Estamos super ansiosos para saber se termos um valente príncipe, ou, uma linda princesa.
+sua presença neste dia é importante para nós.
+Venha e juntos descobriremos!
 
  </p></center>
 
  <div class="container">
 
 <div class="" style="background:#8ccd84;padding:8px;">
-<center>
+<center style="font-size:20px;color:#fff">
 Data/Hora
 </center>
+    <p style="font-size:28px; font-weight:bold">
+25/02/2023
+    </p>
 </div>
  </div>
