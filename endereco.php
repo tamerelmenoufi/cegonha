7ba6ec3805d6e562daea1aaa6e85d38cd4226ac3
livia128924 <livia128924@gmail.com>
2023-01-30 16:53:43 -0300

    atualizando

--- endereco.php
+++ endereco.php
@@ -29,9 +29,9 @@ Data/Hora
     <center style="font-size:20px;color:#fff">
 Localização
     </center>
+</div>
 
-
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3984.131574989324!2d-59.995635685715264!3d-3.0594584409922407!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x926c1a5ae7fdd31b%3A0x38331d4f61e7d3a!2sResidencial%20Quinta%20das%20Laranjeiras!5e0!3m2!1spt-BR!2sbr!4v1675107937155!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
- </div>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3984.131574989324!2d-59.995635685715264!3d-3.0594584409922407!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x926c1a5ae7fdd31b%3A0x38331d4f61e7d3a!2sResidencial%20Quinta%20das%20Laranjeiras!5e0!3m2!1spt-BR!2sbr!4v1675107937155!5m2!1spt-BR!2sbr" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+ 
 
  </div>
