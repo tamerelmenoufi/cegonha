8d665a9f349e28aa7d1517b18d73478e62721999
tamerelmenoufi <tamer@mohatron.com.br>
2023-02-03 10:34:11 -0400

    Atualizações

--- index.php
+++ index.php
@@ -177,6 +177,7 @@
 
         <div class="row gy-4">
           <?php
+          print_r($blq);
             // foreach($p as $i => $v){
             while($d = mysqli_fetch_object($result)){
           ?>
