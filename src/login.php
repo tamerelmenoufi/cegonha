a2e5dea0e43f7db516add267764c1bb7b221d72b
tamerelmenoufi <tamer@mohatron.com.br>
2023-02-05 08:51:26 -0400

    Atualizações

--- src/login.php
+++ src/login.php
@@ -5,9 +5,40 @@ Tela de Login
 
 <button class="btn btn-danger fechar">Fechar</button>
 
+<div class="mb-3">
+    <label for="telefone" class="form-label">Digite o seu número WhatsApp</label>
+    <input type="text" class="form-control" id="telefone" aria-describedby="telefonelHelp">
+    <div id="telefonelHelp" class="form-text">Para lhe identificar, preciso que informe seu número WhatsApp no campo acima.</div>
+</div>
+<button type="submit" class="btn btn-primary enviar">Enviar</button>
+
 <script>
     $(function(){
-
+        $(".enviar").click(function(){
+            telefone = $("#telefone").val();
+            if(telefone.length == 15){
+                $.ajax({
+                    url:"src/login.php",
+                    type:"POST",
+                    data:{
+                        telefone,
+                        acao:'login'
+                    },
+                    success:function(dados){
+                        if(dados.trim() == 'success'){
+                            window.location.href="./?c=<?=md5($d->codigo)?>";
+                        }else{
+                            $.alert({
+                                content:"Ocorreu um erro:<br>Não identificamos o telefone na lista de convidados!",
+                                title:"Alerta de Erro",
+                            });
+                        }
+                    }
+                });
+            }else{
+                $.dialog('Número não informado ou incompleto!');
+            }
+        });
         $(".fechar").click(function(){
             loginPopup.close();
         });
