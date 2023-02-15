3d8ebdad84856fac76159af7b436b5fb4bc8367b
tamerelmenoufi <tamer@mohatron.com.br>
2023-02-14 22:14:54 -0400

    Atualizações

--- painel/src/clientes/convite.php
+++ painel/src/clientes/convite.php
@@ -2,12 +2,12 @@
 
 include("geraConvite.php");
 
-if(is_file("convites/{$_POST['cod']}.png")){
-    $arq = "convites/{$_POST['cod']}.png";
-}else{
-    $arq = GerarConvite($_POST['cod']);
-}
+// if(is_file("convites/{$_POST['cod']}.png")){
+//     $arq = "convites/{$_POST['cod']}.png";
+// }else{
 
+// }
+    $arq = GerarConvite($_POST['cod']);
 
 echo "<img src='src/clientes/{$arq}' style='width:100%' />";
 // unlink($arq);
