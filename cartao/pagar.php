08bd890c40b900cd27f2aecb47d79e203e693891
tamerelmenoufi <tamer@mohatron.com.br>
2023-02-16 17:50:09 -0400

    Atualizações

--- cartao/pagar.php
+++ cartao/pagar.php
@@ -3,4 +3,4 @@
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
 $_POST = json_decode(file_get_contents('php://input'), true);
 
-file_put_contents('x.txt',print_r($_POST, true));
+file_put_contents('x.txt',print_r($_POST, true)."\n\n\n".date("d/m/Y H:i:s"));
