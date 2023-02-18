91e6319aec0e4897d29f8e98ec335723f88da742
tamerelmenoufi <tamer@mohatron.com.br>
2023-02-18 18:41:24 -0400

    Atualizações

--- src/pagar_credito.php
+++ src/pagar_credito.php
@@ -1,3 +1,13 @@
+
+
+<center>
+<br><br><br>
+    <h1>Formato Cartão de Crédito indisponível no momento!</h1>
+</center>
+<?php
+exit();
+?>
+
 <iframe src="cartao/cartao.php" frameborder="1" style="border:0; padding:0; margin:0; width:100%; height:300px;"></iframe>
 
 <script>
