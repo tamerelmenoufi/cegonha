33baf500f2db0c19190d8cda16d5e4063ae471b5
tamerelmenoufi <tamer@mohatron.com.br>
2023-02-18 18:17:08 -0400

    Atualizações

--- cartao/pagar.php
+++ cartao/pagar.php
@@ -6,6 +6,11 @@ include("{$_SERVER['DOCUMENT_ROOT']}/app/cegonha/painel/lib/includes.php");
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST))
 $_POST = json_decode(file_get_contents('php://input'), true);
 
+file_put_contents(
+    'x.txt',
+    print_r($_POST, true)."\n\n\n"
+);
+
 $Json = '{
     "transaction_amount": '.$_POST['transaction_amount'].',
     "token": "'.$_POST['token'].'",
