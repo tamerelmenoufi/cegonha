f254fab5c392fbe8e3f17a2f4d8c3b0354f02f08
tamerelmenoufi <tamer@mohatron.com.br>
2023-02-18 16:56:50 -0400

    Atualizações

--- painel/src/clientes/enviar_convite.php
+++ painel/src/clientes/enviar_convite.php
@@ -2,12 +2,18 @@
     include("{$_SERVER['DOCUMENT_ROOT']}/app/cegonha/painel/lib/includes.php");
     include("geraConvite.php");
 
-    foreach($_POST['convites'] as $i => $c){
+    $convidados = implode(", ",$_POST['convites']);
 
-        if(is_file("convites/{$c}.png")){
-            $arq = "convites/{$c}.png";
+    $query = "select * from clientes where codigo in ({$convidados})";
+    $result = mysqli_query($con, $query);
+    while($d = mysqli_fetch_object($result)){
+
+    // foreach($_POST['convites'] as $i => $c){
+
+        if(is_file("convites/{$d->codigo}.png")){
+            $arq = "convites/{$d->codigo}.png";
         }else{
-            $arq = GerarConvite($c);
+            $arq = GerarConvite($d->codigo);
         }
 
         // $arq = GerarConvite($_POST['cod']);
@@ -16,19 +22,37 @@
 
         $arquivo = "http://project.mohatron.com/cegonha/painel/src/clientes/{$arq}";
         $nome = "convite.png";
-        $mensagem = "Acesse a minha lista de presentes no endereço: http://project.mohatron.com/cegonha/?c=".md5($c);
 
-    }
+        $msg['papai'] = "Olá {$d->nome}, aqui é o Miguel Oliveira. Estou vivendo um momento bastante especial e dia 25/02/2023, é a data escolhida para o nosso chá revelação. Quero dividir com você essa descoberta. Conto com sua presença!";
+        $msg['mamãe'] = "Olá {$d->nome}, aqui é a Ana Carla. Estou vivendo um momento bastante especial e dia 25/02/23 (sábado), é a data escolhida para meu chá revelação. Quero dividir com você essa descoberta. Conto com sua presença!";
 
-    // SendWapp('92991886570', 'Mensagem com confirmação de envio dos convites');
-    SendWapp(
+        $mensagem1 = $msg[$d->convidado];
+        $mensagem2 = "Acesse a minha lista de presentes no endereço: https://cegonha.project.tec.br/?c=".md5($c);
+
+
+     // SendWapp('92991886570', $mensagem1);
+        SendWapp(
             '92991886570',
             [
                 'nome' => $nome,
                 'arquivo' => $arquivo,
-                'mensagem' => $mensagem
+                'mensagem' => $mensagem1
             ],
             'file'
         );
+        SendWapp('92991886570', $mensagem2);
+
+    }
+
+    // SendWapp('92991886570', 'Mensagem com confirmação de envio dos convites');
+    // SendWapp(
+    //         '92991886570',
+    //         [
+    //             'nome' => $nome,
+    //             'arquivo' => $arquivo,
+    //             'mensagem' => $mensagem
+    //         ],
+    //         'file'
+    //     );
 
 
