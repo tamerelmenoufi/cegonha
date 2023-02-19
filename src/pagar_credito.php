b10bf7d56470b55cf36bfc4adc378c9ae57f4d3d
tamerelmenoufi <tamer@mohatron.com.br>
2023-02-19 14:29:08 -0400

    Atualizações

--- src/pagar_credito.php
+++ src/pagar_credito.php
@@ -6,7 +6,7 @@
 // exit();
 ?>
 
-<iframe src="cartao/cartao.php" frameborder="1" style="border:0; padding:0; margin:0; width:100%; height:300px;"></iframe>
+<iframe src="cartao/cartao.php" frameborder="1" style="border:0; padding:0; margin:0; width:100%; height:350px;"></iframe>
 
 <script>
     $(function(){
