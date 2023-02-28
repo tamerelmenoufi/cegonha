672a1f4dd1c0a38c4bb192193fdc5764bf4cab13
tamerelmenoufi <tamer@mohatron.com.br>
2023-02-28 14:48:59 +0200

    Atualizações

--- painel/src/clientes/enviar_agradecimento.php
+++ painel/src/clientes/enviar_agradecimento.php
@@ -14,6 +14,7 @@
     }
 
     // SendWapp('92991886570', 'Enviados '.$qt." convite(s) pelo sistema de mensagem.");
+    SendWapp('92991886570', $mensagem);
     // SendWapp(
     //         '92991886570',
     //         [
